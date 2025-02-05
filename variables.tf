d6d63ce15dd284865d3e28ca98b59126cc29c85a
Vinai <uvr.vinaikumar@gmail.com>
2025-02-05 19:57:51 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -79,3 +79,11 @@ variable "private_route_table_tags" {
 variable "database_route_table_tags" {
     default = {}
 }
+
+variable "is_peering_required" {
+    default = false
+}
+
+variable "vpc_peering_tags" {
+    default = {}
+}
