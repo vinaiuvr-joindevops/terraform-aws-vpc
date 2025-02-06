0bb5c8586aa1069bed9a326b508a2804d75c8478
Vinai <uvr.vinaikumar@gmail.com>
2025-02-06 19:43:08 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,19 @@
 #  output "subnets_info"{
 #     value = aws_subnet.public
 #  }
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids" {
+  value = aws_subnet.database[*].id
+}
